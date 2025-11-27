ff1ac6ad40e6967821d5b6b07a155bb37dd50b40
king-mos <anhminh388373@gmail.com>
2025-11-27 07:01:19 +0700

    up

---
+++ TQM-MOS.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "------------------"
+echo "Đang tạo Packages...."
+apt-ftparchive packages ./DEB > ./Packages;
+#sed -i -e '/^SHA/d' ./Packages;
+bzip2 -c9k ./Packages > ./Packages.bz2;
+echo "------------------"
+echo "Đang tạo Release...."
+printf "Origin: TQM - Mos  ™\nLabel: TQM - Mos  ™\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitectures: iphoneos-arm iphoneos-arm64 iphoneos-arm64e\nComponents: main\nDescription: Kho Lưu Trữu Tinh Chỉnh Tốt Nhất 🤩\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+
+echo "------------------"
+echo "Xong !"
+exit 0;
